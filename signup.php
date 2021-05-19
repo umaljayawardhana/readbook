3f0c22041c19a701df970eb2a7b3d49994911373
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2021-05-19 20:59:48 +0530

    add css

--- signup.php
+++ signup.php
@@ -3,6 +3,55 @@
 <head>
 	<title>	Readbook | Signup</title>
 </head>
+<style >
+	#bar{height: 100px;
+	 background-color: rgb(255,69,1 );
+	  color: white;
+	   font-size: 40px; 
+	   padding: 4px;
+
+	}
+	#signup_button{
+		background-color: #42b72a;	
+		width: 70px;
+		text-align: center;
+		padding: 4px; 
+		border-radius: 4px;
+		float: right; 
+	}
+	#bar2{
+		background-color: white; 
+		font-weight: bold;
+		width: 800px;
+		 height: 580px;
+		 margin: auto; 
+		 margin-top: 50px;"
+		 padding: 10px;
+		 padding-top: 50px;
+		 text-align: center;
+		}
+
+	#text {
+		height: 40px;
+		width: 300px; 
+		border-radius: 4px;
+		border: solid 1px #aaa;
+		padding: 4px;
+		font-size: 14px;
+	}
+	
+	#button {
+		background-color: rgb(255,69,1 );
+		height: 40px;		
+		width: 300px;
+		border-radius: 4px;
+		font-weight: bold;
+		border: none; 
+		color: white;
+	}
+	
+
+</style>
 
 <body style="font-family: tahoma;background-color: #e9ebee;">
 		
