cf873a09703346dd610f798b8d8ad0d308f0d629
Darshana75 <https://github.com/umaljayawardhana/readbook.git>
2021-05-19 20:47:47 +0530

    create signup html

---
+++ signup.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Readbook | Signup</title>
+</head>
+
+<body style="font-family: tahoma;background-color: #e9ebee;">
+		
+		<div id="bar">
+
+			<div style="font-size: 40px;">Readbook</div>
+		
+			<div id="signup_button">Log in</div>
+			</a>
+		</div>
+
+		<div id="bar2">
+			
+			Sign up to Mybook<br><br>
+
+				<input value=">" name="first_name" type="text" id="text" placeholder="First name"><br><br>
+				<input value="<>" name="last_name" type="text" id="text" placeholder="Last name"><br><br>
+
+				<span style="font-weight: normal;">Gender:</span><br>
+				<select id="text" name="gender">
+					
+					<option></option>
+					<option>Male</option>
+					<option>Female</option>
+
+				</select>
+				<br><br>
+				<input value="" name="email" type="text" id="text" placeholder="Email"><br><br>
+				
+				<input name="password" type="password" id="text" placeholder="Password"><br><br>
+				<input name="password2" type="password" id="text" placeholder="Retype Password"><br><br>
+
+				<input type="submit" id="button" value="Sign up">
+				<br><br><br>
+
+		</div>
+
+	</body>
+
+
+</html> 
