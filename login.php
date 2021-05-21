891a8c7205764246750d73cde77f0eee8c37d474
twmickarunasena <twmickarunasena.students.nsbm.ac.lk>
2021-05-21 21:02:47 +0530

    css and htlm add

---
+++ login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Readbook | Log in</title>
+</head>
+<style >
+	#bar{height: 100px;
+	 background-color: rgb(255,69,1 );
+	  color: white;
+	   font-size: 40px; 
+	   padding: 4px;
+
+	}
+	#signup_button{
+		background-color: #42b72a;	
+		width: 70px;
+		text-align: center;
+		padding: 4px; 
+		border-radius: 4px;
+		float: right; 
+	}
+	#bar2{
+		background-color: white; 
+		font-weight: bold;
+		width: 800px;
+		 height: 250px;
+		 margin: auto; 
+		 margin-top: 50px;"
+		 padding: 10px;
+		 padding-top: 50px;
+		 text-align: center;
+		}
+
+	#text {
+		height: 40px;
+		width: 300px;
+		border-radius: 4px;
+		border: solid 1px #aaa;
+		padding: 4px;
+		font-size: 14px;
+	}
+	
+	#button {
+		background-color: rgb(255,69,1 );
+		width: 300px;
+		height: 40px;
+		border-radius: 4px;
+		font-weight: bold;
+		border: none; 
+		color: white;
+	}
+	
+
+</style>
+<body style="font-family: Neue Helvetica; background-color: #e9ebee;">
+	<div id="bar">
+	<div style="font-size: 40px;">Readbook </div>
+	<div style="font-size: 20px;" id="signup_button" >Signup </div>
+	 </div>
+
+	 <div id="bar2">
+	 	Log in to Readbook <br> <br>
+
+	 	<input type="text" placeholder="Email or Username" id="text"> <br> <br>
+	 	<input type="password" placeholder="password" id="text"> <br> <br>
+	 	<input type="submit" id="button" value="Log in">
+
+ 	</div>
+
+</body>
+</html>
