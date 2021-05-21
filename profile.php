f047c0bc998106d6d7c8720cbb2371e0f9867035
Darshana75 <https://github.com/umaljayawardhana/readbook.git>
2021-05-22 00:03:45 +0530

    add profile html

---
+++ profile.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile | Readbook</title>
+</head>
+<body style="font-family: tahoma; background-color: #d0d8e4;">
+	<!--top bar-->
+	<br>
+	<div id="blue_bar">
+		<div style="width: 800px; margin: auto; font-size: 30px;">
+			Readbook  &nbsp &nbsp
+			<input type="text" placeholder="Search for people" id="search_box" >
+			<img src="profilepic.jpg" style="width: 50px; float: right;">  
+		</div>
+	</div>		
+
+	<!--Cover Area-->
+	<br>
+	<div style="width: 800px; margin: auto;  min-height: 400px;">
+		
+		<div style="background-color: white; text-align: center; font-size: 15px; color:  rgb(51, 168, 255)">
+			<img src="miaback.jpg" style="width: 100%;">
+			<img id="profile_pic" src="profilepic.jpg"> 
+			<br>
+			<div style="font-size:25px"> Mia Khalifa  </div>
+			<br>
+			 <div id="menu_button">Timeline </div>
+			 <div id="menu_button">About </div>
+			<div id="menu_button"> Friends </div>
+			<div id="menu_button"> Photoes </div>
+			<div id="menu_button"> Settings</div>
+			 
+		</div>
+		<br>
+
+		<!--below area -->
+		<div style="display: flex;">
+
+			<!--friends area -->
+
+			<div style=" min-height: 400px; flex: 1;">
+
+				<div id="friends_bar">
+					Friends <br>
+
+					<div id="friends">
+						<img id="friends_img" src="user1.jpg"> <br>
+						name_1												
+
+					</div>
+
+					<div id="friends">
+						<img id="friends_img" src="user2.jpg"> <br>
+						name_2												
+
+					</div>
+
+					<div id="friends">
+						<img id="friends_img" src="user3.jpg"> <br>
+						name_3										
+
+					</div>
+
+					<div id="friends">
+						<img id="friends_img" src="user4.jpg"> <br>
+						name_4													
+
+					</div>
+					
+				</div>
+
+			</div>
+
+			<!-- Post Area-->
+
+
+			<div style="min-height: 400px; flex: 2.5; padding: 20px padding-right: 0px; " >
+			
+				 <div style="border: solid thin #aaa;  padding: 10px; background-color:  white; margin-left: 20px; margin-top: 17px;">
+			 	 <textarea  placeholder="Post Your stories ">  </textarea>
+		 	 	 <input type="submit" id="post_button" value="Post">
+		 	 	 <br>
+			 	 	
+			 	
+			 	
+			 	</div>	
+
+			 	<!--posts-->
+
+			 	<div id="post_bar">
+
+			 		<!--posts 01 -->
+
+			 		<div id="post">
+			 			<div>
+			 				<img src="user1.jpg" style="width: 75px; margin-right: 4px;">
+			 			</div>
+			 			<div>
+			 				<div style="font-weight: bold;color: rgb(51, 168, 255);">name_1</div>
+			 				write something
+			 				<br> <br>
+
+			 				<a href="" >Like</a>. <a href="" >Comment</a> . <span style="color: #999;">2021 May 13</span>
+			 			</div>
+			 		</div>
+
+					<!--posts 02 -->
+
+			 		<div id="post">
+			 			<div>
+			 				<img src="user2.jpg" style="width: 75px; margin-right: 4px;">
+			 			</div>
+			 			<div>
+			 				<div style="font-weight: bold;color: rgb(51, 168, 255);">name_2</div>
+			 				write something
+			 				<br> <br>
+
+			 				<a href="" >Like</a>. <a href="" >Comment</a> . <span style="color: #999;">2021 May 13</span>
+			 			</div>
+			 		</div>
+
+			 		
+			 	</div>
+
+			</div>
+		</div>
+	
+	</div>   
+		
+</div>
+</body>
+</html> 
