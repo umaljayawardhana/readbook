4ef12706005ecc0a5658c6b1c9a7f90c9b84af80
Darshana75 <https://github.com/umaljayawardhana/readbook.git>
2021-05-22 19:19:17 +0530

    add timeline html

---
+++ timeline.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile | Readbook</title>
+</head>
+<body style="font-family: tahoma; background-color: #d0d8e4;">
+	<!--top bar-->
+	<br>
+	<div id="blue_bar">
+		<div style="width: 800px; margin: auto; font-size: 30px;">
+			Readbook  &nbsp &nbsp
+			<input type="text" placeholder="Search for people" id="search_box" >
+			<img src="profilepic.jpg" style="width: 50px; float: right;">  
+		</div>
+	</div>		
+
+	<!--Cover Area-->
+	<br>
+	<div style="width: 800px; margin: auto;  min-height: 400px;">
+		
+		
+
+		<!--below area -->
+		<div style="display: flex;">
+
+			<!--friends area -->
+			<div style=" min-height: 400px; flex: 1;">
+
+				<div id="friends_bar">
+
+					<img src="profilepic.jpg" id="profile_pic">
+				 	<br>
+				 	Mia Khalifa
+					
+				</div>
+
+			</div>
+
+			<!-- Post Area-->
+			<div style="min-height: 400px; flex: 2.5; padding: 20px padding-right: 0px; " >
+			
+				 <div style="border: solid thin #aaa;  padding: 10px; background-color:  white; margin-left: 20px; margin-top: 17px;">
+			 	 <textarea  placeholder="Post Your stories ">  </textarea>
+		 	 	 <input type="submit" id="post_button" value="Post">
+		 	 	 <br>
+			 	 	
+			 	
+			 	
+			 	</div>	
+
+			 	<!--posts-->
+
+			 	<div id="post_bar">
+
+			 		<!--posts 01 -->
+
+			 		<div id="post">
+			 			<div>
+			 				<img src="user1.jpg" style="width: 75px; margin-right: 4px;">
+			 			</div>
+			 			<div>
+			 				<div style="font-weight: bold;color: rgb(51, 168, 255);">user2</div>
+			 				Hello write something here .................................................................
+			 				<br> <br>
+
+			 				<a href="" >Like</a>. <a href="" >Comment</a> . <span style="color: #999;">2021 May 13</span>
+			 			</div>
+			 		</div>
+
+					<!--posts 02 -->
+
+			 		<div id="post">
+			 			<div>
+			 				<img src="user2.jpg" style="width: 75px; margin-right: 4px;">
+			 			</div>
+			 			<div>
+			 				<div style="font-weight: bold;color: rgb(51, 168, 255);">user3</div>
+			 				write something here ------------------------------------------------------------------------
+			 				<br> <br>
+
+			 				<a href="" >Like</a>. <a href="" >Comment</a> . <span style="color: #999;">2021 May 13</span>
+			 			</div>
+			 		</div>
+
+			 		
+			 	</div>
+
+			</div>
+		</div>
+	
+	</div>   
+		
+</div>
+</body>
+</html> 
