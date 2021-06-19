48621cc717111aeef6775775002402e065513f76
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2021-06-19 18:09:08 +0530

    user input data sent to database

---
+++ classes/signup.php
@@ -0,0 +1,69 @@
+<?php
+
+ 
+
+class Signup{
+     
+    private $error = "";
+
+    public function evaluate($data){ //data came from signup.php _post array
+
+        foreach ($data as $key => $value) { //check data is empty or not
+            
+            if(empty($value)){
+
+                $this->error = $this->error . $key . "is empty ! <br>";
+            }
+        }
+
+        if($this->error == ""){ //data become a no errors 
+            //no error
+            $this->create_user($data); // run this function
+
+        }else {
+            return $this->error;
+        }
+
+    }
+
+    public function create_user($data){
+        //get data from $data arry "_POST and assign values and keys
+
+        $first_name =$data['first_name'];
+        $last_name =$data['last_name'];
+        $gender =$data['gender'];
+        $email =$data['email'];
+        $password = $data['password'];
+
+        //ceate these 
+        $url_address =strtolower($first_name) . "." .strtolower($last_name); //convert all to lowercase
+        $userid =$this->create_userid(); // run user id function to make random numbers (random user id algo)
+
+
+
+
+        $query ="INSERT INTO users (userid,first_name,last_name,gender,email,password,url_address)
+        VALUES ('$userid','$first_name','$last_name','$gender','$email','$password','$url_address')";
+
+        //connect with database use to connect.php connecion class and save data to DB
+        $DB = new Database();
+        $DB->save($query);
+    }
+
+   
+    private function create_userid(){
+
+        $length = rand(4,19);
+        $number = "";
+        for ($i=0; $i <$length ; $i++) { 
+            $new_rand = rand(0,9);
+            $number = $number . $new_rand;
+        }
+        return $number; 
+    }
+
+}
+
+
+
+?>
