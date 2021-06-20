3da14227a0d86508a97a9d857e49765ab8409c6c
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2021-06-20 14:20:47 +0530

    post data send to database

---
+++ classes/post.php
@@ -0,0 +1,39 @@
+<?php
+
+class Post{
+
+    private $error = "";
+
+    public function create_post($userid, $data){
+
+        if(!empty($data['post'])){
+
+            $post = addslashes($data['post']);
+            $postid = $this->create_postid();
+
+            $query = "insert into posts (userid,postid,post) values ('$userid','$postid','$post')";
+
+            $DB = new Database();
+            $DB->save($query);
+
+            //$id = $_SESSION['readbook_userid'];
+
+        }else {
+            $this->error = "Plese type something! <br>";
+        }
+        return $this->error;
+    }
+    private function create_postid(){
+
+        $length = rand(4,19);
+        $number = "";
+        for ($i=0; $i <$length ; $i++) { 
+            $new_rand = rand(0,9);
+            $number = $number . $new_rand;
+        }
+        return $number; 
+    }
+}
+
+
+?>
