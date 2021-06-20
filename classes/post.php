8731ccccfd41a46758cbd9e66c8946beee143a4c
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2021-06-20 17:07:50 +0530

    displaying post

--- classes/post.php
+++ classes/post.php
@@ -23,6 +23,23 @@ class Post{
         }
         return $this->error;
     }
+
+    public function get_posts($id){
+        $query = "select * from posts where userid = '$id' order by id desc limit 10";
+
+        $DB = new Database();
+        $result = $DB->read($query);
+
+        if($result){
+            return $result;
+        }else{
+            return false;
+        }
+
+
+    }
+
+
     private function create_postid(){
 
         $length = rand(4,19);
@@ -34,6 +51,3 @@ class Post{
         return $number; 
     }
 }
-
-
-?>
