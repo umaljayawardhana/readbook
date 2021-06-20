90aa48b30120745f7d03165ad1f2ecef89a4aaf7
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2021-06-20 11:35:48 +0530

    logout page

--- profile.php
+++ profile.php
@@ -160,8 +160,11 @@
 	<div id="blue_bar">
 		<div style="width: 800px; margin: auto; font-size: 30px;">
 			Readbook  &nbsp &nbsp
-			<input type="text" placeholder="Search for people" id="search_box" >
-			<img src="profilepic.jpg" style="width: 50px; float: right;">  
+			<input type="text" placeholder="Search for people" id="search_box" >	
+			<img src="profilepic.jpg" style="width: 60px;height:50px; float: right;">  
+			<a href="logout.php">
+			<span style="font-size: 20px;float:right;margin:8px;color:white;">Logout</span>
+			</a>
 		</div>
 	</div>		
 
