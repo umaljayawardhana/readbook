adc51677024b0daee43b269d1d9e797fcea98bef
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2021-06-20 18:41:38 +0530

    post design

--- post.php
+++ post.php
@@ -1,16 +1,24 @@
 <div id="post">
         <div>
-            <img src="Cristiano_Ronaldo.jpg" style="width: 75px; margin-right: 4px;">
+        <?php 
+            $image = "images/male.jpg";
+            if($ROW_USER['gender'] == "Female"){
+                $image = "images/female.jpg";
+
+            }
+
+            ?>
+
+            <img src="<?php echo $image ?>" style="width: 75px; margin-right: 4px;">
         </div>
         <div>
-            <div style="font-weight: bold;color: rgb(51, 168, 255);">Cristiano Ronaldo</div>
-            "For me, to watch Messi play is a pleasure – it’s like having an orgasm 
-                – it’s an incredible pleasure. He is always going forwards. He never passes 
-                the ball backwards or sideways.
-                He has only one idea, to run towards the goal. So as a football fan,
-                just enjoy the show."
+            <div style="font-weight: bold;color: rgb(51, 168, 255);">
+            <?php echo $ROW_USER['first_name'] . " " . $ROW_USER['last_name']; ?>
+            </div>
+            <?php echo $ROW['post']?>
             <br> <br>
 
-            <a href="" >Like</a>. <a href="" >Comment</a> . <span style="color: #999;">2021 May 13</span>
+            <a href="" >Like</a>. <a href="" >Comment</a> .
+             <span style="color: #999;"><?php echo $ROW['date'] ?></span>
         </div>
     </div>
