f43b89ef0cf8fc14bdff14e0fd50162cca04d39b
umaljayawardena <mjmkucjayawardhana@gmail.com>
2021-06-22 10:52:17 +0530

    bug fix and tested

--- classes/image.php
+++ classes/image.php
@@ -32,10 +32,78 @@ class Image{
 			}
 		}
 
+		//make width equal to max width;
+		if($max_width != $max_height){
+			if($max_height > $max_width){
+
+				if($max_height > $new_height){
+					$adjustment = ($max_height / $new_height);
+				}else{
+					$adjustment = ($new_height / $max_height);
+
+				}
+				$new_width = $new_width * $adjustment;
+				$new_height =$new_height * $adjustment;
+
+			}else{
+
+				if($max_width > $new_width){
+					$adjustment = ($max_width / $new_width);
+				}else{
+					$adjustment = ($new_width / $max_width);
+
+				}
+				$new_width = $new_width * $adjustment;
+				$new_height =$new_height * $adjustment;
+			}
+		}
+
         $new_image = imagecreatetruecolor($new_width, $new_height);
         imagecopyresampled($new_image, $original_image, 0, 0, 0,0, $new_width, $new_height,$original_width, $original_height);
 
-        imagejpeg($new_image,$cropped_file_name,75);
+		imagedestroy($original_image);
+
+		if($max_width != $max_height){
+
+			if($max_width > $max_height){
+
+				$diff = ($new_height - $max_height);
+				if($diff < 0){
+					$diff = $diff * -1;
+				}
+				$y = round($diff / 2);
+				$x = 0;
+			}else{
+				
+				$diff = ($new_width - $max_height);
+				if($diff < 0){
+					$diff = $diff * -1;
+				}
+				$x = round($diff / 2);
+				$y = 0;
+			}
+
+		}
+		else{
+
+			if($new_height > $new_width){
+
+				$diff = ($new_height - $new_width);
+				$y = round($diff / 2);
+				$x = 0;
+			}else{
+				
+				$diff = ($new_width - $new_height);
+				$x = round($diff / 2);
+				$y = 0;
+			}
+		}
+
+		$new_cropped_image = imagecreatetruecolor($max_width, $max_height);
+		imagecopyresampled($new_cropped_image, $new_image, 0, 0, $x, $y, $max_width, $max_height,$max_width,$max_height);
+		imagedestroy($new_image);
+        imagejpeg($new_cropped_image,$cropped_file_name,75);
+		imagedestroy($new_cropped_image);
 
         
     }
