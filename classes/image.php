4980711c004d5b09579320af5ca25db48abcf541
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2021-06-22 10:49:57 +0530

    cover picture finalized

--- classes/image.php
+++ classes/image.php
@@ -32,21 +32,71 @@ class Image{
 			}
 		}
 
+		//make width equal to max width;
+		if($max_width != $max_height){
+			if($max_height > $max_width){
+
+				if($max_height > $new_height){
+					$adjustment = ($max_height / $new_height);
+				}else{
+					$adjustment = ($new_height / $max_height);
+
+				}
+				$new_width = $new_width * $adjustment;
+				$new_height =$new_height * $adjustment;
+
+			}else{
+
+				if($max_width > $new_width){
+					$adjustment = ($max_width / $new_width);
+				}else{
+					$adjustment = ($new_width / $max_width);
+
+				}
+				$new_width = $new_width * $adjustment;
+				$new_height =$new_height * $adjustment;
+			}
+		}
+
         $new_image = imagecreatetruecolor($new_width, $new_height);
         imagecopyresampled($new_image, $original_image, 0, 0, 0,0, $new_width, $new_height,$original_width, $original_height);
 
 		imagedestroy($original_image);
 
-		if($new_height > $new_width){
+		if($max_width != $max_height){
 
-			$diff = ($new_height - $new_width);
-			$y = round($diff / 2);
-			$x = 0;
-		}else{
-			
-			$diff = ($new_width - $new_height);
-			$x = round($diff / 2);
-			$y = 0;
+			if($max_width > $max_height){
+
+				$diff = ($new_height - $max_height);
+				if($diff < 0){
+					$diff = $diff * -1;
+				}
+				$y = round($diff / 2);
+				$x = 0;
+			}else{
+				
+				$diff = ($new_width - $max_height);
+				if($diff < 0){
+					$diff = $diff * -1;
+				}
+				$x = round($diff / 2);
+				$y = 0;
+			}
+
+		}
+		else{
+
+			if($new_height > $new_width){
+
+				$diff = ($new_height - $new_width);
+				$y = round($diff / 2);
+				$x = 0;
+			}else{
+				
+				$diff = ($new_width - $new_height);
+				$x = round($diff / 2);
+				$y = 0;
+			}
 		}
 
 		$new_cropped_image = imagecreatetruecolor($max_width, $max_height);
