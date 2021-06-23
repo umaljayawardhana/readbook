d8d4baaf5db72763fb1483a982a9ddf7fb6c75c9
Kelum <72620573+kelumpriyamantha@users.noreply.github.com>
2021-06-23 21:25:46 +0530

    classes/post.php

--- classes/post.php
+++ classes/post.php
@@ -1,278 +1,455 @@
 <?php 
 
-class Image
+class Post
 {
+	private $error = "";
 
-	public function generate_filename($length)
+	public function create_post($userid, $data, $files, $owner = 0)
 	{
-
-		$array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-		$text = "";
-
-		for($x = 0; $x < $length; $x++)
+ 		
+		if(!empty($data['post']) || !empty($files['file']['name']) || isset($data['is_profile_image']) || isset($data['is_cover_image']))
 		{
 
-			$random = rand(0,61);
-			$text .= $array[$random];
-		}
+			$myimage = "";
+			$has_image = 0;
+			$is_cover_image = 0;
+			$is_profile_image = 0;
 
-		return $text;
-	}
-
-	public function crop_image($original_file_name,$cropped_file_name,$max_width,$max_height)
-	{
-
-		if(file_exists($original_file_name))
-		{
- 
-			$original_image = imagecreatefromjpeg($original_file_name);
-
-			$original_width = imagesx($original_image);
-			$original_height = imagesy($original_image);
-
-			if($original_height > $original_width)
+			if(isset($data['is_profile_image']) || isset($data['is_cover_image']))
 			{
-				//make width equal to max width;
-				$ratio = $max_width / $original_width;
 
-				$new_width = $max_width;
-				$new_height = $original_height * $ratio;
+				$myimage = $files;
+				$has_image = 1;
 
+				if(isset($data['is_cover_image']))
+				{
+					$is_cover_image = 1;
+				}
+
+				if(isset($data['is_profile_image']))
+				{
+					$is_profile_image = 1;
+				}
 			}else
 			{
 
-				//make width equal to max width;
-				$ratio = $max_height / $original_height;
+				if(!empty($files['file']['name']))
+				{
 
-				$new_height = $max_height;
-				$new_width = $original_width * $ratio;
+
+					$folder = "uploads/" . $userid . "/";
+
+						//create folder
+						if(!file_exists($folder))
+						{
+
+							mkdir($folder,0777,true);
+							file_put_contents($folder . "index.php", "");
+						}
+					
+					$allowed[] = "image/jpeg";
+					$allowed[] = "video/mp4";
+
+					if(in_array($files['file']['type'], $allowed)){
+
+						$image_class = new Image();
+
+						$ext = pathinfo($files['file']['name'],PATHINFO_EXTENSION);
+						$ext = strtolower($ext);
+
+						$myimage = $folder . $image_class->generate_filename(15) . "." . $ext;
+						move_uploaded_file($files['file']['tmp_name'], $myimage);
+
+						if($ext == "jpg" || $ext == "jpeg"){
+							$image_class->resize_image($myimage,$myimage,1500,1500);
+						}
+
+						$has_image = 1;
+					}else{
+
+						$this->error .= "The selected image is not a valid type. only jpegs allowed!<br>";
+					}
+				}
 			}
-		}
 
-		//adjust incase max width and height are different
-		if($max_width != $max_height)
-		{
+			$post = "";
+			if(isset($data['post'])){
 
-			if($max_height > $max_width)
-			{
-
-				if($max_height > $new_height)
-				{
-					$adjustment = ($max_height / $new_height);
-				}else
-				{
-					$adjustment = ($new_height / $max_height);
-				}
-
-				$new_width = $new_width * $adjustment;
-				$new_height = $new_height * $adjustment;
-			}else
-			{
-
-				if($max_width > $new_width)
-				{
-					$adjustment = ($max_width / $new_width);
-				}else
-				{
-					$adjustment = ($new_width / $max_width);
-				}
-
-				$new_width = $new_width * $adjustment;
-				$new_height = $new_height * $adjustment;
+				$post = addslashes($data['post']);
 			}
-		}
 
-		$new_image = imagecreatetruecolor($new_width, $new_height);
-		imagecopyresampled($new_image, $original_image, 0, 0, 0, 0, $new_width, $new_height, $original_width, $original_height);
+			//add tagged users
+			$tags = array();
+			$tags = get_tags($post);
+			$tags = json_encode($tags);
 
-		imagedestroy($original_image);
+			if($this->error == ""){
 
-		if($max_width != $max_height)
-		{
+				$postid = $this->create_postid();
+				$parent = 0;
+				$DB = new Database();
 
-			if($max_width > $max_height)
-			{
+				if(isset($data['parent']) && is_numeric($data['parent'])){
 
-				$diff = ($new_height - $max_height);
-				if($diff < 0){
-					$diff = $diff * -1;
+					$parent = $data['parent'];
+					$mypost = $this->get_one_post($data['parent']);
+
+					if(is_array($mypost) && $mypost['userid'] != $userid){
+						
+						//follow this item
+						content_i_follow($userid,$mypost);
+
+						//add notification
+						add_notification($_SESSION['readbook_userid'],"comment",$mypost);
+					}
+
+					$sql = "update posts set comments = comments + 1 where postid = '$parent' limit 1";
+					$DB->save($sql);
 				}
-				$y = round($diff / 2);
-				$x = 0;
-			}else
-			{
 
-				$diff = ($new_width - $max_height);
-				if($diff < 0){
-					$diff = $diff * -1;
-				}
-				$x = round($diff / 2);
-				$y = 0;
+				$query = "insert into posts (owner,userid,postid,post,image,has_image,is_profile_image,is_cover_image,parent,tags) values ('$owner','$userid','$postid','$post','$myimage','$has_image','$is_profile_image','$is_cover_image','$parent','$tags')";
+				$DB->save($query);
+
+				//notify those that were tagged
+				tag($postid);
+
 			}
 		}else
 		{
-			if($new_height > $new_width)
-			{
-
-				$diff = ($new_height - $new_width);
-				$y = round($diff / 2);
-				$x = 0;
-			}else
-			{
-
-				$diff = ($new_width - $new_height);
-				$x = round($diff / 2);
-				$y = 0;
-			}
+			$this->error .= "Please type something to post!<br>";
 		}
 
-		$new_cropped_image = imagecreatetruecolor($max_width, $max_height);
-		imagecopyresampled($new_cropped_image, $new_image, 0, 0, $x, $y, $max_width, $max_height, $max_width, $max_height);
-		
-		imagedestroy($new_image);
-
-		imagejpeg($new_cropped_image,$cropped_file_name,90);
-		imagedestroy($new_cropped_image);
+		return $this->error;
 	}
 
-	//resize the image
-	public function resize_image($original_file_name,$resized_file_name,$max_width,$max_height)
+	public function edit_post($data, $files)
 	{
 
-		if(file_exists($original_file_name))
+		if(!empty($data['post']) || !empty($files['file']['name']))
 		{
 
-			$original_image = imagecreatefromjpeg($original_file_name);
+			$myimage = "";
+			$has_image = 0;
 
-			$original_width = imagesx($original_image);
-			$original_height = imagesy($original_image);
+ 				if(!empty($files['file']['name']))
+				{
 
-			if($original_height > $original_width)
-			{
-				//make width equal to max width;
-				$ratio = $max_width / $original_width;
 
-				$new_width = $max_width;
-				$new_height = $original_height * $ratio;
+					$folder = "uploads/" . $userid . "/";
 
-			}else
-			{
+						//create folder
+						if(!file_exists($folder))
+						{
 
-				//make width equal to max width;
-				$ratio = $max_height / $original_height;
+							mkdir($folder,0777,true);
+							file_put_contents($folder . "index.php", "");
+						}
 
-				$new_height = $max_height;
-				$new_width = $original_width * $ratio;
+						$image_class = new Image();
+
+						$myimage = $folder . $image_class->generate_filename(15) . ".jpg";
+						move_uploaded_file($_FILES['file']['tmp_name'], $myimage);
+
+						$image_class->resize_image($myimage,$myimage,1500,1500);
+
+					$has_image = 1;
+				}
+ 
+			$post = "";
+			if(isset($data['post'])){
+
+				$post = addslashes($data['post']);
 			}
-		}
 
-		//adjust incase max width and height are different
-		if($max_width != $max_height)
+			$postid = addslashes($data['postid']);
+
+			if($has_image){
+				$query = "update posts set post = '$post', image = '$myimage' where postid = '$postid' limit 1";
+			}else{
+				$query = "update posts set post = '$post' where postid = '$postid' limit 1";
+			}
+
+			//notify those that were tagged
+			tag($postid, $post);
+
+			$DB = new Database();
+			$DB->save($query);
+
+		}else
 		{
-
-			if($max_height > $max_width)
-			{
-
-				if($max_height > $new_height)
-				{
-					$adjustment = ($max_height / $new_height);
-				}else
-				{
-					$adjustment = ($new_height / $max_height);
-				}
-
-				$new_width = $new_width * $adjustment;
-				$new_height = $new_height * $adjustment;
-			}else
-			{
-
-				if($max_width > $new_width)
-				{
-					$adjustment = ($max_width / $new_width);
-				}else
-				{
-					$adjustment = ($new_width / $max_width);
-				}
-
-				$new_width = $new_width * $adjustment;
-				$new_height = $new_height * $adjustment;
-			}
+			$this->error .= "Please type something to post!<br>";
 		}
 
-		$new_image = imagecreatetruecolor($new_width, $new_height);
-		imagecopyresampled($new_image, $original_image, 0, 0, 0, 0, $new_width, $new_height, $original_width, $original_height);
-
-		imagedestroy($original_image);
-
-		imagejpeg($new_image,$resized_file_name,90);
-		imagedestroy($new_image);
+		return $this->error;
 	}
 
-		//create thumbnail for cover image
-		public function get_thumb_cover($filename)
-		{
 	
-			$thumbnail = $filename . "_cover_thumb.jpg";
-			if(file_exists($thumbnail))
-			{
-				return $thumbnail;
-			}
-	
-			$this->crop_image($filename,$thumbnail,1366,488);
-	
-			if(file_exists($thumbnail))
-			{
-				return $thumbnail;
-			}else
-			{
-				return $filename;
-			}
+
+	public function get_posts($id,$post_type = "profile")
+	{
+
+		$page_number = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+  		$page_number = ($page_number < 1) ? 1 : $page_number;
+
+		$limit = 10;
+ 		$offset = ($page_number - 1) * $limit;
+
+		$query = "select * from posts where parent = 0 and owner = 0 and userid = '$id' order by id desc limit $limit offset $offset";
+		
+		if($post_type == "group"){
+			$query = "select * from posts where parent = 0 and owner = '$id' order by id desc limit $limit offset $offset";
 		}
-	
-		//create thumbnail for profile image
-		public function get_thumb_profile($filename)
+
+		$DB = new Database();
+		$result = $DB->read($query);
+
+		if($result)
 		{
-	
-			$thumbnail = $filename . "_profile_thumb.jpg";
-			if(file_exists($thumbnail))
-			{
-				return $thumbnail;
-			}
-	
-			$this->crop_image($filename,$thumbnail,600,600);
-	
-			if(file_exists($thumbnail))
-			{
-				return $thumbnail;
-			}else
-			{
-				return $filename;
-			}
-		}
-	
-		//create thumbnail for post image
-		public function get_thumb_post($filename)
+			return $result;
+		}else
 		{
-	
-			$thumbnail = $filename . "_post_thumb.jpg";
-			if(file_exists($thumbnail))
-			{
-				return $thumbnail;
-			}
-	
-			$this->crop_image($filename,$thumbnail,600,600);
-	
-			if(file_exists($thumbnail))
-			{
-				return $thumbnail;
-			}else
-			{
-				return $filename;
-			}
+			return false;
 		}
-	
-	
 	}
 
-?>
 
+	public function get_comments($id)
+	{
+
+		$page_number = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+  		$page_number = ($page_number < 1) ? 1 : $page_number;
+
+		$limit = 10;
+ 		$offset = ($page_number - 1) * $limit;
+
+		$query = "select * from posts where parent = '$id' order by id asc limit $limit offset $offset";
+
+		$DB = new Database();
+		$result = $DB->read($query);
+
+		if($result)
+		{
+			return $result;
+		}else
+		{
+			return false;
+		}
+	}
+
+	public function get_one_post($postid)
+	{
+
+		if(!is_numeric($postid)){
+
+			return false;
+		}
+
+		$query = "select * from posts where postid = '$postid' limit 1";
+
+		$DB = new Database();
+		$result = $DB->read($query);
+
+		if($result)
+		{
+			return $result[0];
+		}else
+		{
+			return false;
+		}
+	}
+
+
+	public function delete_post($postid)
+	{
+
+		if(!is_numeric($postid)){
+			
+			return false;
+		}
+
+		$Post = new Post();
+		$one_post = $Post->get_one_post($postid);
+
+		$DB = new Database();
+		$sql = "select parent from posts where postid = '$postid' limit 1";
+		$result = $DB->read($sql);
+		
+		if(is_array($result)){
+
+			if($result[0]['parent'] > 0){
+
+				$parent = $result[0]['parent'];
+
+				$sql = "update posts set comments = comments - 1 where postid = '$parent' limit 1";
+				$DB->save($sql);
+			}
+		}
+			
+
+		$query = "delete from posts where postid = '$postid' limit 1";
+		$DB->save($query);
+
+		//delete any images and thumbnails
+		if($one_post['image'] != "" && file_exists($one_post['image']))
+		{
+			unlink($one_post['image']);
+		}
+
+		if($one_post['image'] != "" && file_exists($one_post['image']. "_post_thumb"))
+		{
+			unlink($one_post['image']. "_post_thumb");
+		}
+
+		if($one_post['image'] != "" && file_exists($one_post['image']. "_cover_thumb"))
+		{
+			unlink($one_post['image']. "_cover_thumb");
+		}
+
+		//delete all comments
+		$query = "delete from posts where parent = '$postid' ";
+		$DB->save($query);
+
+
+  
+	}
+
+	public function i_own_post($postid,$readbook_userid)
+	{
+
+		if(!is_numeric($postid)){
+			
+			return false;
+		}
+
+		$query = "select * from posts where postid = '$postid' limit 1";
+
+		$DB = new Database();
+		$result = $DB->read($query);
+  		
+  		if(is_array($result)){
+
+  			if($result[0]['userid'] == $readbook_userid){
+
+  				return true;
+  			}
+  		}
+
+  		return false;
+	}
+
+	public function get_likes($id,$type){
+
+		$DB = new Database();
+		$type = addslashes($type);
+
+		if(is_numeric($id)){
+ 
+			//get like details
+			$sql = "select likes from likes where type='$type' && contentid = '$id' limit 1";
+			$result = $DB->read($sql);
+			if(is_array($result)){
+
+				$likes = json_decode($result[0]['likes'],true);
+				return $likes;
+			}
+		}
+
+
+		return false;
+	}
+
+	public function like_post($id,$type,$readbook_userid){
+
+
+ 			$DB = new Database();
+ 			
+			//save likes details
+			$sql = "select likes from likes where type='$type' && contentid = '$id' limit 1";
+			$result = $DB->read($sql);
+			if(is_array($result)){
+
+				$likes = json_decode($result[0]['likes'],true);
+
+				$user_ids = array_column($likes, "userid");
+ 
+				if(!in_array($readbook_userid, $user_ids)){
+
+					$arr["userid"] = $readbook_userid;
+					$arr["date"] = date("Y-m-d H:i:s");
+
+					$likes[] = $arr;
+
+					$likes_string = json_encode($likes);
+					$sql = "update likes set likes = '$likes_string' where type='$type' && contentid = '$id' limit 1";
+					$DB->save($sql);
+
+					//increment the right table
+					$sql = "update {$type}s set likes = likes + 1 where {$type}id = '$id' limit 1";
+					$DB->save($sql);
+
+					if($type != "user"){
+						$post = new Post();
+						$single_post = $post->get_one_post($id);
+
+						//add notification
+						add_notification($_SESSION['readbook_userid'],"like",$single_post);
+					}
+
+				}else{
+
+					$key = array_search($readbook_userid, $user_ids);
+					unset($likes[$key]);
+
+					$likes_string = json_encode($likes);
+					$sql = "update likes set likes = '$likes_string' where type='$type' && contentid = '$id' limit 1";
+					$DB->save($sql);
+
+					//increment the right table
+					$sql = "update {$type}s set likes = likes - 1 where {$type}id = '$id' limit 1";
+					$DB->save($sql);
+
+				}
+				
+
+			}else{
+
+				$arr["userid"] = $readbook_userid;
+				$arr["date"] = date("Y-m-d H:i:s");
+
+				$arr2[] = $arr;
+
+				$likes = json_encode($arr2);
+				$sql = "insert into likes (type,contentid,likes) values ('$type','$id','$likes')";
+				$DB->save($sql);
+
+				//increment the right table
+				$sql = "update {$type}s set likes = likes + 1 where {$type}id = '$id' limit 1";
+				$DB->save($sql);
+ 
+ 				if($type != "user"){
+	 				$post = new Post();
+					$single_post = $post->get_one_post($id);
+
+					//add notification
+					add_notification($_SESSION['readbook_userid'],"like",$single_post);
+				}
+			}
+
+	}
+
+
+	private function create_postid()
+	{
+
+		$length = rand(4,19);
+		$number = "";
+		for ($i=0; $i < $length; $i++) { 
+			# code...
+			$new_rand = rand(0,9);
+
+			$number = $number . $new_rand;
+		}
+
+		return $number;
+	}
+}
