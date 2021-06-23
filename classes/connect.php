3cb0cf574db783286d4dd09491a83eb4e712629a
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2021-06-23 22:53:40 +0530

    classes/connect.php

--- classes/connect.php
+++ classes/connect.php
@@ -3,24 +3,11 @@
 class Database
 {
 
-	private $host = "localhost"; //default
-	private $username = "root"; // if ur db hasnt username this one is default
-	private $password = "";  // default
-	private $db = "readbook_db"; //which database name do you to access
-
-	/*---Test quary ----
-	$connection = mysqli_connect($host,$username,$password,$db);
-	//$first_name = "chanuka";
-	//$last_name = "mora";
-	$query = "SELECT * FROM users";
-	$result = mysqli_query($connection,$query); 
-	print_r($result);
-	while($row = mysqli_fetch_all($result)){
-		echo "<pre>";
-		print_r($row);
-		echo "</pre>";
-	} */
-	
+	private $host = "localhost";
+	private $username = "root";
+	private $password = "";
+	private $db = "readbook_db";
+ 
 	function connect()
 	{
 
@@ -65,21 +52,4 @@ class Database
 		}
 	}
 
-}
-/* -------write to database test---------
-$DB = new Database();
-$query = "INSERT INTO users (first_name, last_name) VALUES ('umal', 'jayawardhana')";
-$DB->save($query); 
-
-connect to database and read and write sql in db please create dabase "readbook_db"
-	
---- Read from database test ----
-$DB = new Database();
-
-$query = "SELECT * FROM users";
-$data = $DB->read($query);
-echo "<pre>";
-print_r($data);
-echo "</pre>"; */
-
-
+} 
