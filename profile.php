089b0967a7160b8e28649dc2c6a436ddef22fd31
twmickarunasena <twmickarunasena.students.nsbm.ac.lk>
2021-06-24 10:54:32 +0530

    profile

--- profile.php
+++ profile.php
@@ -1,307 +1,436 @@
 <?php
 
-
-
-	session_start();
-	//unset($_SESSION['readbook_userid']); //logout
-
-	//print_r($_SESSION);
-	//$_SESSION['readbook_userid'] == "";
-	include("classes/connect.php");
-	include("classes/login.php");
-	include("classes/user.php");
-	include("classes/post.php");
-	include("classes/image.php");
-
+	include("classes/autoload.php");
 
 	$login = new Login();
-	$user_data = $login->check_login($_SESSION['readbook_userid']);
+	$_SESSION['readbook_userid'] = isset($_SESSION['readbook_userid']) ? $_SESSION['readbook_userid'] : 0;
+	
+	$user_data = $login->check_login($_SESSION['readbook_userid'],false);
+ 
+ 	$USER = $user_data;
+ 	
+ 	if(isset($URL[1]) && is_numeric($URL[1])){
 
-	//for posting starts here 
-	if($_SERVER['REQUEST_METHOD'] == "POST"){
+	 	$profile = new Profile();
+	 	$profile_data = $profile->get_profile($URL[1]);
+
+	 	if(is_array($profile_data)){
+	 		$user_data = $profile_data[0];
+	 	}
+
+ 	}
+ 	
+	//posting starts here
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+
+		include("change_image.php");
 		
+		if(isset($_POST['first_name'])){
 
-		$post = new Post();
-		
-		$id = $_SESSION['readbook_userid'];
-		$result = $post->create_post($id, $_POST,$_FILES);
+			$settings_class = new Settings();
+			$settings_class->save_settings($_POST,$_SESSION['readbook_userid']);
 
-		if($result == ""){
-			header("Location: profile.php");
-			die;
-		
+		}elseif(isset($_POST['post'])){
 
-		//print_r($_POST);
-		}else{
-				echo "<div style='text-align:center;font-size:12px;color:white;background-color:grey";
+			$post = new Post();
+			$id = $_SESSION['readbook_userid'];
+			$result = $post->create_post($id, $_POST,$_FILES);
+			
+			if($result == "")
+			{
+				header("Location: " . ROOT . "profile");
+				die;
+			}else
+			{
+
+				echo "<div style='text-align:center;font-size:12px;color:white;background-color:grey;'>";
 				echo "<br>The following errors occured:<br><br>";
 				echo $result;
 				echo "</div>";
+			}
 		}
+			
 	}
 
-	//collect post
-
+	//collect posts
 	$post = new Post();
-		
-	$id = $_SESSION['readbook_userid'];
+	$id = $user_data['userid'];
+	
 	$posts = $post->get_posts($id);
 
 	//collect friends
 	$user = new User();
-		
-	$id = $_SESSION['readbook_userid'];
-	$friends = $user->get_friends($id);
+ 	
+	$friends = $user->get_following($user_data['userid'],"user");
 
 	$image_class = new Image();
 
+	//check if this is from a notification
+	if(isset($URL[2])){
+		notification_seen($URL[2]);
+	}
+
 ?>
 
-
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Profile | Readbook</title>
-</head>
-<style>
+	<html>
+	<head>
+		<title>Profile | readbook</title>
+	</head>
 
-
-
-	#blue_bar{
-		height: 50px;
-		background-color: rgb(51, 168, 255);
-		color: #d9dfeb;
-	}
-
-	#search_box {
-		border-radius: 5px;
-		width: 400px;
-		height: 20px;
-		border: none;
-		padding: 4px;
-		background-image: url(search.png);
-		background-repeat: no-repeat;
-		background-position: right;
+	<style type="text/css">
 		
-	}
+		#blue_bar{
 
-	#profile_pic {
-		width: 150px;
-		margin-top: -200px;
-		border-radius: 50%;
-		border: solid 2px white;
-	}
+			height: 50px;
+			background-color: rgb(51, 168, 255);
+			color: #d9dfeb;
 
-	#menu_button {
-		width: 100px;
-		display: inline-block;
-		margin: 2px;
-	}
+		}
 
-	#friends_img {
-		width: 75px;
-		float: left;
-		margin: 8px;
-		height: 75px;
-	}
+		#search_box{
 
-	#friends_bar {
-		font-size: 15px;
+			width: 400px;
+			height: 20px;
+			border-radius: 5px;
+			border:none;
+			padding: 4px;
+			font-size: 14px;
+			background-image: url(search.png);
+			background-repeat: no-repeat;
+			background-position: right;
 
-		background-color: white;
-		min-height: 400px;
-		margin-top: 20px;
-		color: #aaa;
-		padding: 8px;		
-	}
+		}
 
-	#friends {
-		clear: both;
-		font-size: 12px;
-		font-weight: bold;
-		color:  rgb(51, 168, 255);
+		#textbox{
 
-	}
+			width: 100%;
+			height: 20px;
+			border-radius: 5px;
+			border:none;
+			padding: 4px;
+			font-size: 14px;
+			border: solid thin grey;
+			margin:10px;
+ 
+		}
 
-	textarea {
-		width: 100%;
-		border: none;
-		font-family: tahoma;
-		font-size: 14px;
-		height: 60px ;
+		#profile_pic{
 
+			width: 150px;
+			margin-top: -300px;
+			border-radius: 50%;
+			border:solid 2px white;
+		}
 
+		#menu_buttons{
 
-	}
+			width: 100px;
+			display: inline-block;
+			margin:2px;
+		}
 
-	#post_button {
-		float: right;
-		background-color:  rgb(51, 168, 255);
-		border: none;
-		color: white;
-		padding: 4px;
-		font-size: 14px;
-		border-radius: 2px;
-		width: 50px;
-	}
+		#friends_img{
 
+			width: 75px;
+			float: left;
+			margin:8px;
 
-	#post_bar {
-		margin-top: 20px;
-		margin-left: 20px;
-		background-color: white;
-		padding: 10px;
-	}
+		}
 
-	#post {
-		padding: 4px;
-		font-size: 13px;
-		display: flex;
-		margin-bottom: 20px;
-	}
+		#friends_bar{
 
+			background-color: white;
+			min-height: 400px;
+			margin-top: 20px;
+			color: #aaa;
+			padding: 8px;
+		}
 
-	
-</style>
-<body style="font-family: tahoma; background-color: #d0d8e4;">
-	<!--top bar-->
-	<br>
-	<?php
-	include("header.php");
-	?>
+		#friends{
 
-	<!--Cover Area-->
-	<br>
-	<div style="width: 800px; margin: auto;  min-height: 400px;">
-		
-		<div style="background-color: white; text-align: center; font-size: 15px; color:  rgb(51, 168, 255)">
+		 	clear: both;
+		 	font-size: 12px;
+		 	font-weight: bold;
+		 	color: rgb(51, 168, 255);
+		}
 
-			<?php
+		textarea{
 
-				$image = "images/cover_image.jpg";
-				if(file_exists($user_data['cover_image'])){
-					$image = $image_class->get_thumb_cover($user_data['cover_image']);
-				}
-			?>	
-			<img src="<?php echo $image?>" style="width: 100%;">
-			<span style="font-size: 12px;">
+			width: 100%;
+			border:none;
+			font-family: tahoma;
+			font-size: 14px;
+			height: 60px;
 
-				<?php
+		}
 
-					$image = "images/male.jpg";
-					if($user_data['gender'] == "Female"){
-						$image = "images/female.jpg";
-					}
-					if(file_exists($user_data['profile_image'])){
-						$image = $image_class->get_thumb_profile($user_data['profile_image']); 
-					}
-				?>	
+		#post_button{
 
-			 <img id="profile_pic" src="<?php echo $image?>"> </br>
+			float: right;
+			background-color: rgb(51, 168, 255);
+			border:none;
+			color: white;
+			padding: 4px;
+			font-size: 14px;
+			border-radius: 2px;
+			width: 50px;
+			min-width: 50px;
+			cursor: pointer;
+		}
+ 
+ 		#post_bar{
 
-			 <a style="text-decoration: none; color:#f0f ;" href="change_profile_image.php?change=profile"> Change Profile Image </a> |
-			 <a style="text-decoration: none; color:#f0f ;" href="change_profile_image.php?change=cover"> Change Cover </a>
-			 </span>
-			<br>
-			<div style="font-size:25px"> <?php echo $user_data['first_name']. " ". $user_data['last_name'] ?>  </div>
-			<br>
-			<a href="index.php"> <div id="menu_button">Timeline </div></a>
-			 <div id="menu_button">About </div>
-			<div id="menu_button"> Friends </div>
-			<div id="menu_button"> Photoes </div>
-			<div id="menu_button"> Settings</div>
-			 
-		</div>
+ 			margin-top: 20px;
+ 			background-color: white;
+ 			padding: 10px;
+ 		}
+
+ 		#post{
+
+ 			padding: 4px;
+ 			font-size: 13px;
+ 			display: flex;
+ 			margin-bottom: 20px;
+ 		}
+
+	</style>
+
+	<body style="font-family: tahoma; background-color: #d0d8e4;">
+
 		<br>
+		<?php include("header.php"); ?>
+ 
+ 		<!--change profile image area-->
+ 		<div id="change_profile_image" style="display:none;position:absolute;width: 100%;height: 100%;background-color: #000000aa;">
+ 			<div style="max-width:600px;margin:auto;min-height: 400px;flex:2.5;padding: 20px;padding-right: 0px;">
+ 					
+ 					<form method="post" action="<?=ROOT?>profile/profile" enctype="multipart/form-data">
+	 					<div style="border:solid thin #aaa; padding: 10px;background-color: white;">
 
-		<!--below area -->
-		<div style="display: flex;">
+	 						<input type="file" name="file"><br>
+	 						<input id="post_button" type="submit" style="width:120px;" value="Change">
+	 						<br>
+							<div style="text-align: center;">
+								<br><br>
+							<?php
 
-			<!--friends area -->
+								echo "<img src='" . ROOT . "$user_data[profile_image]' style='max-width:500px;' >";
+  
+	 						?>
+							</div>
+	 					</div>
+  					</form>
 
-			<div style=" min-height: 400px; flex: 1;">
-
-				<div id="friends_bar">
-					Friends <br>
-
-
-					<?php
-					 if($friends){
-
-						foreach ($friends as $FRIEND_ROW) {
-							# code...
-							//$user = new User();
-							//$ROW_USER = $user->get_user($ROW['userid']);
-
-							include("user.php");
-						}
-					 }
-
-	
-					 ?>
-			 		
-
-					
-
-				
-					
-				</div>
-
-			</div>
-
-			<!-- Post Area-->
-
-
-			<div style="min-height: 400px; flex: 2.5; padding: 20px; padding-right: 0px; " >
-			
-				 <div style="border: solid thin #aaa;  padding: 10px; background-color:  white;">
-
-				 <form method="post" enctype="multipart/form-data">
-
-			 	 	<textarea name="post"  placeholder="whats on my mind ">  </textarea>
-					<input type="file" name="file">
-		 	 	 	<input type="submit" id="post_button" value="Post">
-		 	 	 	<br>
-				 </form>
-			 	 	
-			 	
-			 	
-			 	</div>	
-
-			 	<!--posts-->
-
-			 	<div id="post_bar">
-
-			 		<!--posts 01 -->
-
-					 <?php
-					 if($posts){
-
-						foreach ($posts as $ROW) {
-							# code...
-							$user = new User();
-							$ROW_USER = $user->get_user($ROW['userid']);
-
-							include("post.php");
-						}
-					 }
-
-	
-					 ?>
-			 		
-
-					<!--posts 02 -->
-
-			 		
-
-			 		
-			 	</div>
-
-			</div>
-		</div>
-	
-	</div>   
+ 				</div>
+ 		</div>
 		
-</div>
-</body>
-</html> 
+		<!--change cover image area-->
+ 		<div id="change_cover_image" style="display:none;position:absolute;width: 100%;height: 100%;background-color: #000000aa;">
+ 			<div style="max-width:600px;margin:auto;min-height: 400px;flex:2.5;padding: 20px;padding-right: 0px;">
+ 					
+ 					<form method="post" action="<?=ROOT?>profile/cover" enctype="multipart/form-data">
+	 					<div style="border:solid thin #aaa; padding: 10px;background-color: white;">
+
+	 						<input type="file" name="file"><br>
+	 						<input id="post_button" type="submit" style="width:120px;" value="Change">
+	 						<br>
+							<div style="text-align: center;">
+								<br><br>
+							<?php
+
+ 	 							echo "<img src='" . ROOT . "$user_data[cover_image]' style='max-width:500px;' >";
+								 
+	 						?>
+							</div>
+	 					</div>
+  					</form>
+
+ 				</div>
+ 		</div>
+
+		<!--cover area-->
+		<div style="width: 800px;margin:auto;min-height: 400px;">
+			
+			<div style="background-color: white;text-align: center;color: rgb(51, 168, 255)">
+
+					<?php 
+
+						$image = "images/cover_image.jpg";
+						if(file_exists($user_data['cover_image']))
+						{
+							$image = $image_class->get_thumb_cover($user_data['cover_image']);
+						}
+					?>
+
+				<img src="<?php echo ROOT . $image ?>" style="width:100%;">
+
+
+				<span style="font-size: 12px;">
+					<?php 
+
+						$image = "images/user_male.jpg";
+						if($user_data['gender'] == "Female")
+						{
+							$image = "images/user_female.jpg";
+						}
+						if(file_exists($user_data['profile_image']))
+						{
+							$image = $image_class->get_thumb_profile($user_data['profile_image']);
+						}
+					?>
+
+					<img id="profile_pic" src="<?php echo ROOT . $image ?>"><br/>
+
+					<?php if(i_own_content($user_data)):?>
+					
+						<a onclick="show_change_profile_image(event)" style="text-decoration: none;color:#f0f;" href="<?=ROOT?>change_profile_image/profile">Change Profile Image</a> | 
+						<a onclick="show_change_cover_image(event)" style="text-decoration: none;color:#f0f;" href="<?=ROOT?>change_profile_image/cover">Change Cover</a>
+					
+					<?php endif; ?>
+
+				</span>
+				<br>
+					<div style="font-size: 20px;color: black;">
+						<a href="<?=ROOT?>profile/<?php echo $user_data['userid'] ?>">
+							<?php echo $user_data['first_name'] . " " . $user_data['last_name']  ?>
+							<br><span style="font-size:12px;">@<?=$user_data['tag_name']?></span>
+						</a>
+
+						<?php 
+							$mylikes = "";
+							if($user_data['likes'] > 0){
+
+								$mylikes = "(" . $user_data['likes'] . " Followers)";
+							}
+						?>
+						<br>
+
+						<a href="<?=ROOT?>like/user/<?php echo $user_data['userid'] ?>">
+							<input id="post_button" type="button" value="Follow <?php echo $mylikes ?>" style="margin-right:10px;background-color: #9b409a;width:auto;">
+						</a>
+
+						<?php if($user_data['userid'] == $_SESSION['readbook_userid']): ?>
+							<a href="<?=ROOT?>messages">
+								<input id="post_button" type="button" value="Messages" style="margin-right:10px;background-color: #1b9186;width:auto;">
+							</a>
+						<?php else: ?>
+							<a href="<?=ROOT?>messages/new/<?=$user_data['userid']?>">
+								<input id="post_button" type="button" value="Message" style="margin-right:10px;background-color: #1b9186;width:auto;">
+							</a>
+						<?php endif; ?>
+ 						
+
+					</div>
+				<br>
+				<br>
+
+
+				<a href="<?=ROOT?>home"><div id="menu_buttons">Timeline</div></a>
+				<a href="<?=ROOT?>profile/<?php echo $user_data['userid'] ?>/about"><div id="menu_buttons">About</div></a>
+				<a href="<?=ROOT?>profile/<?php echo $user_data['userid'] ?>/followers"><div id="menu_buttons">Followers</div></a>
+				<a href="<?=ROOT?>profile/<?php echo $user_data['userid'] ?>/following"><div id="menu_buttons">Following</div></a>
+				<a href="<?=ROOT?>profile/<?php echo $user_data['userid'] ?>/photos"><div id="menu_buttons">Photos</div></a>
+				
+				<?php 
+					if($user_data['userid'] == $_SESSION['readbook_userid']){
+						
+						echo '<a href="'.ROOT. 'profile/'.$user_data['userid'].'/groups"><div id="menu_buttons">Groups</div></a>';
+						echo '<a href="'.ROOT. 'profile/'.$user_data['userid'].'/settings"><div id="menu_buttons">Settings</div></a>';
+					}
+				?>
+			</div>
+
+			<!--below cover area-->
+	 
+	 		<?php 
+
+	 			$section = "default";
+
+	 			if(isset($URL[2])){
+
+	 				$section = $URL[2];
+	 			}
+
+	 			if($section == "default"){
+
+	 				include("profile_content_default.php");
+	 			 
+	 			}elseif($section == "following"){
+	 				
+	 				include("profile_content_following.php");
+
+	 			}elseif($section == "followers"){
+
+	 				include("profile_content_followers.php");
+
+	 			}elseif($section == "about"){
+
+	 				include("profile_content_about.php");
+
+	 			}elseif($section == "settings"){
+
+	 				include("profile_content_settings.php");
+
+	 			}elseif($section == "photos"){
+
+	 				include("profile_content_photos.php");
+	 			}elseif($section == "groups"){
+
+	 				include("profile_content_groups.php");
+	 			}
+
+
+
+	 		?>
+
+		</div>
+
+	</body>
+</html>
+
+<script type="text/javascript">
+	
+	function show_change_profile_image(event){
+
+		event.preventDefault();
+		var profile_image = document.getElementById("change_profile_image");
+		profile_image.style.display = "block";
+	}
+
+
+	function hide_change_profile_image(){
+
+		var profile_image = document.getElementById("change_profile_image");
+		profile_image.style.display = "none";
+	}
+
+	
+	function show_change_cover_image(event){
+
+		event.preventDefault();
+		var cover_image = document.getElementById("change_cover_image");
+		cover_image.style.display = "block";
+	}
+
+
+	function hide_change_cover_image(){
+
+		var cover_image = document.getElementById("change_cover_image");
+		cover_image.style.display = "none";
+	}
+
+
+	window.onkeydown = function(key){
+
+		if(key.keyCode == 27){
+
+			//esc key was pressed
+			hide_change_profile_image();
+			hide_change_cover_image();
+		}
+	}
+
+	
+</script>
